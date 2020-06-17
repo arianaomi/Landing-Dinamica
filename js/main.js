5d48936a0f714b2e67e6dc1be4ccbed351e8bd52
Arianaomi <arianaomi.lp@gmail.com>
2020-06-17 17:44:23 -0500

    :zap: Agregando cards
    Agregando cards

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 /* Variales globales */
 var koderObject = {};
 
-/* Obteniendo los datos del formulario */
+/* Hacer html dinamico */
+const loadContent = (contentUrl) => {
+  $("#container-wrapper").load(contentUrl);
+};
+//$("#kodersCard").on("click",loadContent("../cards.html"));
 
+/* Hacer html dinamico */
+
+/* Obteniendo los datos del formulario */
 const getKoderForm = () => {
   let date = new Date();
   console.log(date);
@@ -11,8 +18,6 @@ const getKoderForm = () => {
     koderObject[datakoder.name] = datakoder.value;
     koderObject["newDate"] = date;
   });
-  console.log(koderObject);
-
   saveKoder(koderObject);
   $("#saveModal").modal("hide");
 };
@@ -23,14 +28,12 @@ $("#save-koder").click(getKoderForm);
 
 /* Request */
 const saveKoder = (koderObject) => {
-  //$("#saveModal").modal("hide");
   $.ajax({
     url: "https://ajaxclass-1ca34.firebaseio.com/Equipo2/koders/.json",
-    method: "POSt",
+    method: "POST",
     data: JSON.stringify(koderObject),
     success: (response) => {
       let responseVal = response.name;
-      console.log(responseVal);
       $("#successModal")
         .find(".modal-body")
         .append(`Su clave de registro es: ${responseVal}`);
@@ -39,3 +42,34 @@ const saveKoder = (koderObject) => {
     },
   });
 };
+
+const printCard = (koderObject) => {
+  let { name, lastName, age, biography, photo } = koderObject;
+  $("#cardWrapper").append(`<div class="col-12 col-md-4 my-3">
+      <div class="card">
+      <img class="card-img-top" src="${photo}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="">${name} ${lastName}</h5>
+        <h6>${age}</h4>
+        <p class="card-text">${biography}</p>
+      </div>
+    </div>
+  </div>`);
+};
+
+const getKodersDb = () => {
+  $.get(
+    "https://ajaxclass-1ca34.firebaseio.com/Equipo2/koders/.json",
+    function (response) {
+      console.log(response);
+      $.each(response, (key, value) => {
+        console.log(value);
+        printCard(value);
+      });
+      //printCard(response);
+    }
+  );
+};
+
+$("#kodersCard").click(getKodersDb);
+//getKodersDb();
